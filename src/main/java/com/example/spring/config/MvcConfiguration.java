883033b2d24c106afdc778d170c00a9dc6093818
mastroianni <davide.mastroianni@euris.it>
2020-07-08 17:27:01 +0200

    rimosso refuso userController

--- src/main/java/com/example/spring/config/MvcConfiguration.java
+++ src/main/java/com/example/spring/config/MvcConfiguration.java
@@ -11,7 +11,7 @@ public class MvcConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        //registry.addViewController("/user").setViewName("user");
+        registry.addViewController("/user").setViewName("user");
         registry.addViewController("/login").setViewName("login");
     }
 
