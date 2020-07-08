b2c6848c6be7a0c5957a1eebf2b805afd92adc58
mastroianni <davide.mastroianni@euris.it>
2020-07-08 14:53:50 +0200

    pulizia codice

--- src/main/java/com/example/spring/config/SecurityConfig.java
+++ src/main/java/com/example/spring/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package com.example.spring.config;
 
-import com.example.spring.entities.Users;
-import com.example.spring.repositories.UserRepository;
-import com.example.spring.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,22 +7,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private UserRepository userRepository;
-
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -55,19 +43,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
             .and().csrf().disable();
     }
-
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//            User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
-
 
 }
--- src/main/java/com/example/spring/controllers/UserController.java
+++ src/main/java/com/example/spring/controllers/UserController.java
@@ -1,18 +1,9 @@
 package com.example.spring.controllers;
 
-import java.util.List;
-import java.util.Optional;
-
 import com.example.spring.repositories.UserRepository;
-import com.example.spring.exceptions.UsersNotFoundException;
-import com.example.spring.entities.Users;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
@@ -30,14 +21,5 @@ public class UserController {
     public String user() {
         return "user";
     }
-//
-//    @RequestMapping(value = "userId", method = RequestMethod.GET)
-//    public ModelAndView user(Model model) {
-//        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        Optional<Users> user = repository.findByUsername(userDetails.getUsername());
-//        ModelAndView mav = new ModelAndView("userId");
-//        mav.addObject("messages", user.get().getId());
-//        return mav;
-//    }
 
 }
